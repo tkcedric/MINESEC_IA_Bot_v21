929b8e5696491235a87798a7ad719ad60e7f333b
@tkcedric <tkcedric2002@gmail.com>
2025-07-14 20:24:45 +0100

    coorestion of build

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+# Installer les dépendances système
 apt-get update
 apt-get install -y pandoc texlive-xetex texlive-latex-base texlive-latex-extra dvipng cm-super fonts-liberation fonts-dejavu
+
+# Configurer les polices
 mkdir -p /usr/share/fonts/truetype/custom
 ln -s /usr/share/fonts/truetype/liberation /usr/share/fonts/truetype/custom/
 fc-cache -f -v
+
+# Installer les dépendances Python
 pip install -r requirements.txt
-echo "Vérification des polices..."
-fc-list | grep DejaVu
+
+# Convertir le notebook en script (sécurité)
+jupyter nbconvert --to script minesec_bot.ipynb
